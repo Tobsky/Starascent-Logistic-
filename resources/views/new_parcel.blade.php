3b786fe2051ba09cdaa8e94a04cf8c4e87d42d9b
Abdullateef <lateef4dem@gmail.com>
2021-12-19 16:48:04 +0800

    Added requests to New Parcel

--- resources/views/new_parcel.blade.php
+++ resources/views/new_parcel.blade.php
@@ -16,9 +16,9 @@
             
             <form class="user content-justify-center" method="POST" action="new_parcel" >
                 @csrf
-                <div class="row">    
-                    <div class="col-lg-6">
-                        <div class="p-5">
+                <div class="row row-cols-1">    
+                    <div class="col-md-6 pb-2 pt-5 px-5">
+                        <div >
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Pick-up Information</h1>
                             </div>
@@ -44,7 +44,7 @@
                                         @enderror  
                                 </div>
                                 <div class="form-group">
-                                        <input type="text" class="form-control @error('sender_contact') is-invalid @enderror"
+                                        <input type="tel" class="form-control @error('sender_contact') is-invalid @enderror"
                                             id="sender_contact" placeholder="Pickup mobile number" name="sender_contact" value="" required>
 
                                         @error('sender_contact')
@@ -68,8 +68,9 @@
 
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                        <div class="p-5">
+                    <hr class="d-md-none d-lg-none col-9 offset-1">
+                    <div class="col-md-6 pb-2 pt-5 px-5">
+                        <div>
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Recipient Information</h1>
                             </div>
@@ -95,7 +96,7 @@
                                     @enderror
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" class="form-control @error('recipient_contact') is-invalid @enderror"
+                                    <input type="tel" class="form-control @error('recipient_contact') is-invalid @enderror"
                                         id="recipient_contact" placeholder="Delivery mobile number" name="recipient_contact" value="" required>
 
                                     @error('recipient_contact')
@@ -107,17 +108,19 @@
                             </div>
                         </div>
                     </div>
-                
+                    <div class="col-10 offset-1 col-md-10 offset-md-1 pb-3">
+                        <textarea class="form-control" name="comment" id="" rows="3" placeholder="Request & Comments"></textarea>
+                    </div>
                     
                 </div>    
-                <div class="form-group row">
-                    <div class="col-sm-6 mb-3 mb-sm-0">
+                <div class="form-group row offset-1">
+                    <div class="col-5 mb-3 ml-3">
                         <button type="submit" class="btn btn-primary btn-user btn-block ">
                             Save
                         </button>
                     </div>
-                    <div class="col-sm-6">
-                        <a type="submit" class="btn btn-secondary btn-user btn-block " href="{{ url('parcel_list') }}"> Cancel</a>
+                    <div class="col-5 mb-3 mr-3 ">
+                        <a type="submit" class="btn btn-secondary btn-user btn-block" href="{{ url('parcel_list') }}"> Cancel</a>
                     </div>
                     
                 </div>
